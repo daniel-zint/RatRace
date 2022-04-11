d81133077b07e04cbcd712d215739e18e9c7fce0
Daniel Zint <daniel.zint@inria.fr>
2022-04-11 17:16:44 +0200

    format files. No change in the code.

--- src/RatRace/RatRace.h
+++ src/RatRace/RatRace.h
@@ -1,42 +1,42 @@
 #pragma once
 
-#include <glog/logging.h>
-#include <experimental/filesystem>
-
-#include "MeshHeader.h"
-#include "DualHybridGraph.h"
-#include "PolySurfMesh.h"
 #include "../BackgroundGrid/SizeGrid.h"
+#include "DualHybridGraph.h"
+#include "MeshHeader.h"
+#include "PolySurfMesh.h"
 
-class RatRace
-{
-	PolySurfMesh mesh_;
-	real featureAngle_ = 30;
+#include <experimental/filesystem>
+#include <glog/logging.h>
 
-	OpenMesh::VPropHandleT<bool> vRemesh;
-	OpenMesh::VPropHandleT<bool> vRemeshNew;
-	OpenMesh::VPropHandleT<int> vValence;
+class RatRace {
+    PolySurfMesh mesh_;
+    real featureAngle_ = 30;
 
-public:
-	void initBlossomQuad( TriMesh& tmesh );
-	void initTriangleMerging( TriMesh& tmesh );
-	void initHybrid( TriMesh& tmesh );
-	void run();
-	void postProcessing(const int& nQuads, SizeGrid* sizegrid = nullptr, const std::experimental::filesystem::path& outputPath = std::experimental::filesystem::path() );
-	auto& mesh() { return mesh_; }
+    OpenMesh::VPropHandleT<bool> vRemesh;
+    OpenMesh::VPropHandleT<bool> vRemeshNew;
+    OpenMesh::VPropHandleT<int> vValence;
 
-private:
-	int swapValence();
-	int removeInteriorValence2Vertices();
-	int valence2SplitBoundary();
-	int lowQualSplitBoundary();
-	int removeBoundaryValence2Vertices();
-	int collapse3x3y( SizeGrid* sizegrid = nullptr );
-	int valenceSplit( int nQuadsMax = INT_MAX, SizeGrid* sizegrid = nullptr );
+  public:
+    void initBlossomQuad( TriMesh& tmesh );
+    void initTriangleMerging( TriMesh& tmesh );
+    void initHybrid( TriMesh& tmesh );
+    void run();
+    void postProcessing( const int& nQuads, SizeGrid* sizegrid = nullptr,
+                         const std::experimental::filesystem::path& outputPath = std::experimental::filesystem::path() );
+    auto& mesh() { return mesh_; }
 
-	void initRemeshProps();
-	void swapRemeshProps();
-	void deleteRemeshProps();
+  private:
+    int swapValence();
+    int removeInteriorValence2Vertices();
+    int valence2SplitBoundary();
+    int lowQualSplitBoundary();
+    int removeBoundaryValence2Vertices();
+    int collapse3x3y( SizeGrid* sizegrid = nullptr );
+    int valenceSplit( int nQuadsMax = INT_MAX, SizeGrid* sizegrid = nullptr );
 
-	int boundaryValence( const OpenMesh::SmartVertexHandle& vh );
+    void initRemeshProps();
+    void swapRemeshProps();
+    void deleteRemeshProps();
+
+    int boundaryValence( const OpenMesh::SmartVertexHandle& vh );
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,181 +1,174 @@
 #include <experimental/filesystem>
 // glog
-#include <glog/logging.h>
-
+#include "BackgroundGrid/SizeGrid.h"
+#include "MeshOptimization.h"
+#include "QualityMetrics.h"
+#include "RatRace.h"
+#include "Stopwatch/Stopwatch.h"
 #include "workingDirectory.h"
 
-#include "RatRace.h"
-#include "MeshOptimization.h"
-
-#include "QualityMetrics.h"
-#include "Stopwatch/Stopwatch.h"
-#include "BackgroundGrid/SizeGrid.h"
+#include <glog/logging.h>
 
 namespace fs = std::experimental::filesystem;
 
-//template<int = 0> void discreteMeshOptimization( PolyMesh& mesh, const float grid_scale = 0.5f, int n_iter = 100 );
-
 void printQuality( PolyMesh& mesh, const fs::path& outputPath ) {
-	// print quality information
-	auto nVal2 = 0;
-	for( auto vh : mesh.vertices() ) {
-		auto fh = *( mesh.vf_begin( vh ) );
-		std::vector<PolyMesh::Point> points;
-		points.reserve( 4 );
-		for( auto fv : mesh.fv_range( fh ) ) {
-			points.push_back( mesh.point( fv ) );
-		}
-		auto q = QualityMetrics::conditionSurf( points );
-		if( mesh.valence( vh ) == 2 && q < 0.01 ) {
-			++nVal2;
-			LOG( INFO ) << "Low quality vertex: " << vh.idx() << " | " << mesh.point( vh );
-			if( !vh.is_boundary() )
-				LOG( INFO ) << "   Vertex is not on boundary!!!";
-		}
-	}
-	LOG( INFO ) << "Number of valence 2 vertices: " << nVal2;
-	std::vector<int> qVec( 10, 0 );
-	real qMin = std::numeric_limits<real>::max(), qMean = 0;
-	for( auto fh : mesh.faces() ) {
-		std::vector<PolyMesh::Point> points;
-		points.reserve( 4 );
-		for( auto fv : mesh.fv_range( fh ) ) {
-			points.push_back( mesh.point( fv ) );
-		}
-		auto q = QualityMetrics::conditionSurf( points );
+    // print quality information
+    auto nVal2 = 0;
+    for( auto vh: mesh.vertices() ) {
+        auto fh = *( mesh.vf_begin( vh ) );
+        std::vector<PolyMesh::Point> points;
+        points.reserve( 4 );
+        for( auto fv: mesh.fv_range( fh ) ) {
+            points.push_back( mesh.point( fv ) );
+        }
+        auto q = QualityMetrics::conditionSurf( points );
+        if( mesh.valence( vh ) == 2 && q < 0.01 ) {
+            ++nVal2;
+            LOG( INFO ) << "Low quality vertex: " << vh.idx() << " | " << mesh.point( vh );
+            if( !vh.is_boundary() )
+                LOG( INFO ) << "   Vertex is not on boundary!!!";
+        }
+    }
+    LOG( INFO ) << "Number of valence 2 vertices: " << nVal2;
+    std::vector<int> qVec( 10, 0 );
+    real qMin = std::numeric_limits<real>::max(), qMean = 0;
+    for( auto fh: mesh.faces() ) {
+        std::vector<PolyMesh::Point> points;
+        points.reserve( 4 );
+        for( auto fv: mesh.fv_range( fh ) ) {
+            points.push_back( mesh.point( fv ) );
+        }
+        auto q = QualityMetrics::conditionSurf( points );
 
-		qMin = std::min( q, qMin );
-		qMean += q;
+        qMin = std::min( q, qMin );
+        qMean += q;
 
-		if( q < 0.1 )
-			LOG( INFO ) << "Low quality at " << points[0];
+        if( q < 0.1 )
+            LOG( INFO ) << "Low quality at " << points[0];
 
-		if( q < 0 ) {
-			++qVec[0];
-		} else
-			++qVec[( q * qVec.size() ) - 0.0001];
-	}
-	qMean /= mesh.n_faces();
-	LOG( INFO ) << "qMin = " << qMin;
-	LOG( INFO ) << "qMean = " << qMean;
-	for( int i = 0; i < qVec.size(); ++i ) {
-		LOG( INFO ) << "Quality " << (double)( i ) / qVec.size() << " - " << (double)( i + 1 ) / qVec.size() << "\t|\t" << qVec[i];
-	}
-	
-	//mesh.request_face_colors();
-	//for( auto fh : mesh.faces() ) {
-	//	auto q = QualityMetrics::conditionSurf( mesh, fh );
-	//	PolyMesh::Point c = { 0,0,0 };
-	//	if( q > 0.1 )
-	//		c = PolyMesh::Point( 255 ) * q;
-	//	else
-	//		//c[0] = 200 * -q + 55;
-	//		c[0] = 255;
-	//	rr.mesh().set_color( fh, { c[0],c[1],c[2] } );
-	//}
-	//OpenMesh::IO::write_mesh( mesh, ( outputPath / "quality.off" ).string(), OpenMesh::IO::Options::FaceColor, 10 );
+        if( q < 0 ) {
+            ++qVec[0];
+        } else
+            ++qVec[( q * qVec.size() ) - 0.0001];
+    }
+    qMean /= mesh.n_faces();
+    LOG( INFO ) << "qMin = " << qMin;
+    LOG( INFO ) << "qMean = " << qMean;
+    for( int i = 0; i < qVec.size(); ++i ) {
+        LOG( INFO ) << "Quality " << (double)( i ) / qVec.size() << " - " << (double)( i + 1 ) / qVec.size() << "\t|\t" << qVec[i];
+    }
 
-	// count valence
-	int nVal3 = 0, nVal4 = 0, nVal5 = 0;
-	for( auto vh : mesh.vertices() ) {
-		if( vh.is_boundary() )
-			continue;
-		auto val = vh.valence();
-		if( val < 4 )
-			++nVal3;
-		else if( val == 4 )
-			++nVal4;
-		else
-			++nVal5;
-	}
-	LOG( INFO ) << "Valences: " << nVal3 << " < 4 | " << nVal4 << " == 4 | " << nVal5 << " > 4";
-	LOG( INFO ) << "singular vertices [%]: " << 100 * (float)( nVal3 + nVal5 ) / (float)( nVal3 + nVal5 + nVal4 );
+    // mesh.request_face_colors();
+    // for( auto fh : mesh.faces() ) {
+    //	auto q = QualityMetrics::conditionSurf( mesh, fh );
+    //	PolyMesh::Point c = { 0,0,0 };
+    //	if( q > 0.1 )
+    //		c = PolyMesh::Point( 255 ) * q;
+    //	else
+    //		//c[0] = 200 * -q + 55;
+    //		c[0] = 255;
+    //	rr.mesh().set_color( fh, { c[0],c[1],c[2] } );
+    //}
+    // OpenMesh::IO::write_mesh( mesh, ( outputPath / "quality.off" ).string(), OpenMesh::IO::Options::FaceColor, 10 );
+
+    // count valence
+    int nVal3 = 0, nVal4 = 0, nVal5 = 0;
+    for( auto vh: mesh.vertices() ) {
+        if( vh.is_boundary() )
+            continue;
+        auto val = vh.valence();
+        if( val < 4 )
+            ++nVal3;
+        else if( val == 4 )
+            ++nVal4;
+        else
+            ++nVal5;
+    }
+    LOG( INFO ) << "Valences: " << nVal3 << " < 4 | " << nVal4 << " == 4 | " << nVal5 << " > 4";
+    LOG( INFO ) << "singular vertices [%]: " << 100 * (float)( nVal3 + nVal5 ) / (float)( nVal3 + nVal5 + nVal4 );
 }
 
-int main( int argc, char* argv[] ) 
-{
-	//////////////////////////////////////////////////
-	//                                              //
-	//                 *** Init ***                 //
-	//                                              //
-	//----------------------------------------------//
+int main( int argc, char* argv[] ) {
+    //////////////////////////////////////////////////
+    //                                              //
+    //                 *** Init ***                 //
+    //                                              //
+    //----------------------------------------------//
 
-	// --- init logging ---
-	google::InitGoogleLogging( argv[0] );
-	//FLAGS_timestamp_in_logfile_name = false;
-	FLAGS_logtostderr = true;
-		
-	// --- paths ---
-	fs::path basePath = WORKING_DIRECTORY;
-	LOG( INFO ) << "basePath = " << fs::canonical( basePath );
-	LOG_ASSERT( fs::exists( basePath ) );
-	std::string meshName = "transition4to1";
-	fs::path inputPath = basePath / "meshes";
-	fs::path outputPath = basePath / "output";
-	fs::path cachePath = basePath / "cache";
+    // --- init logging ---
+    google::InitGoogleLogging( argv[0] );
+    // FLAGS_timestamp_in_logfile_name = false;
+    FLAGS_logtostderr = true;
 
-	fs::create_directories( outputPath );
+    // --- paths ---
+    fs::path basePath = WORKING_DIRECTORY;
+    LOG( INFO ) << "basePath = " << fs::canonical( basePath );
+    LOG_ASSERT( fs::exists( basePath ) );
+    std::string meshName = "transition4to1";
+    fs::path inputPath   = basePath / "meshes";
+    fs::path outputPath  = basePath / "output";
+    fs::path cachePath   = basePath / "cache";
 
-	// uncomment for logging to output path
-	//FLAGS_logtostderr = false;
-	//FLAGS_alsologtostderr = true;
-	//FLAGS_log_dir = outputPath.string();
+    fs::create_directories( outputPath );
 
-	fs::path meshFile = inputPath / (meshName + ".off");
+    // uncomment for logging to output path
+    // FLAGS_logtostderr = false;
+    // FLAGS_alsologtostderr = true;
+    // FLAGS_log_dir = outputPath.string();
 
-	LOG( INFO ) << "Mesh: " << meshFile;
+    fs::path meshFile = inputPath / ( meshName + ".off" );
 
+    LOG( INFO ) << "Mesh: " << meshFile;
 
-	RatRace rr;
-	TriMesh initMesh;
-	OpenMesh::IO::read_mesh( initMesh, meshFile.string() );
-	OpenMesh::IO::write_mesh( initMesh, ( outputPath / "triangleMesh.off" ).string() );
-	const auto nQuads = initMesh.n_faces() / 2;
-	LOG( INFO ) << "nQuads = " << nQuads;
+    RatRace rr;
+    TriMesh initMesh;
+    OpenMesh::IO::read_mesh( initMesh, meshFile.string() );
+    OpenMesh::IO::write_mesh( initMesh, ( outputPath / "triangleMesh.off" ).string() );
+    const auto nQuads = initMesh.n_faces() / 2;
+    LOG( INFO ) << "nQuads = " << nQuads;
 
+    //////////////////////////////////////////////////
+    //                                              //
+    //             *** Tri to Quad ***              //
+    //                                              //
+    //----------------------------------------------//
 
-	//////////////////////////////////////////////////
-	//                                              //
-	//             *** Tri to Quad ***              //
-	//                                              //
-	//----------------------------------------------//
+    // Stopwatch sw;
+    // sw.start();
 
-	//Stopwatch sw;
-	//sw.start();
+    // rr.initBlossomQuad( initMesh );			// Blossom-Quad
+    // rr.initTriangleMerging( initMesh );		// Triangle Merging
+    rr.initHybrid( initMesh );    // Hybrid Approach
 
-	//rr.initBlossomQuad( initMesh );			// Blossom-Quad
-	//rr.initTriangleMerging( initMesh );		// Triangle Merging
-	rr.initHybrid( initMesh );				// Hybrid Approach
+    OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "hybridMesh.off" ).string() );
+    LOG( INFO ) << "Start RatRace";
 
-	OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "hybridMesh.off" ).string() );
-	LOG( INFO ) << "Start RatRace";
+    // --- merge remaining triangles ---
+    rr.run();
 
-	// --- merge remaining triangles ---
-	rr.run();
+    // sw.stop();
+    // LOG( INFO ) << "Runtime: " << sw.runtimeStr<Stopwatch::Milliseconds>();
+    LOG( INFO ) << "Finish RatRace";
+    OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "quadMesh.off" ).string() );
 
-	//sw.stop();
-	//LOG( INFO ) << "Runtime: " << sw.runtimeStr<Stopwatch::Milliseconds>();
-	LOG( INFO ) << "Finish RatRace";
-	OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "quadMesh.off" ).string() );
+    //////////////////////////////////////////////////
+    //                                              //
+    //           *** post-processing ***            //
+    //                                              //
+    //----------------------------------------------//
 
-	//////////////////////////////////////////////////
-	//                                              //
-	//           *** post-processing ***            //
-	//                                              //
-	//----------------------------------------------//
+    SizeGrid sizegrid = loadSizeGrid( initMesh, cachePath, meshFile, 1000, 1000 );
+    rr.postProcessing( nQuads, &sizegrid );
+    // rr.postProcessing( nQuads, &sizegrid, outputPath ); // use this for more intermediate results
 
-	SizeGrid sizegrid = loadSizeGrid( initMesh, cachePath, meshFile, 1000, 1000 );
-	rr.postProcessing( nQuads, &sizegrid );
-	// rr.postProcessing( nQuads, &sizegrid, outputPath ); // use this for more intermediate results
+    //////////////////////////////////////////////////
+    //                                              //
+    //          *** print quality info ***          //
+    //                                              //
+    //----------------------------------------------//
 
-	//////////////////////////////////////////////////
-	//                                              //
-	//          *** print quality info ***          //
-	//                                              //
-	//----------------------------------------------//
-	
-	printQuality( rr.mesh(), outputPath );
-	OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "finalMesh.off" ).string(), OpenMesh::IO::Options::Default, 10 );
+    printQuality( rr.mesh(), outputPath );
+    OpenMesh::IO::write_mesh( rr.mesh(), ( outputPath / "finalMesh.off" ).string(), OpenMesh::IO::Options::Default, 10 );
 
-	LOG( INFO ) << "RatRace finished";
+    LOG( INFO ) << "RatRace finished";
 }
